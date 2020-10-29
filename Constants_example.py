7072c0c690c593fddaeb89b3e1acf80018df4da9
Samuel Mertenat <samuel.mertenat@gmail.com>
2020-10-29 11:54:16 +0100

    get video

--- Constants_example.py
+++ Constants_example.py
@@ -2,3 +2,5 @@ COURSE_NAME       = ''
 
 API_BASE_URL      = ''
 API_BEARER        = ''
+
+VIMEO_BASE_URL    = 'https://player.vimeo.com/video/'
--- MyGuitareCourseParser.py
+++ MyGuitareCourseParser.py
@@ -3,10 +3,6 @@
 import requests
 import Constants
 
-VIDEO_BASE_URL    = ''
-
-
-
 COURSE_MODULES_URL = '/course-modules/'
 LESSONS_URL       = '/lessons/'
 
@@ -25,31 +21,31 @@ if response.status_code == 200:
   courseModule_slug = ''
   userCourseModuleLesson_slug = ''
 
-  """
+  
   for userCourseModule in json_response['data']['userCourseModules']['data']:
     courseModule_slug = userCourseModule['courseModule']['data']['slug']
     for userCourseModuleLesson in userCourseModule['userCourseModuleLessons']['data']:
       userCourseModuleLesson_slug = userCourseModuleLesson['lesson']['data']['slug']
 
-      lesson_url = API_BASE_URL + COURSE_NAME + COURSE_MODULES_URL + courseModule_slug + LESSONS_URL + userCourseModuleLesson_slug
-      
+      lesson_url = Constants.API_BASE_URL + Constants.COURSE_NAME + COURSE_MODULES_URL + courseModule_slug + LESSONS_URL + userCourseModuleLesson_slug
+    
       response = requests.patch(
         lesson_url,
         headers = {
           'Accept': 'application/json',
-          'authorization': 'Bearer ' + API_BEARER
+          'authorization': 'Bearer ' + Constants.API_BEARER
         },
         params = {'action': 'start'},
       )
       if response.status_code == 200:
         json_response = response.json()
-
+        
         lesson_title = str(json_response['data']['position']).zfill(2) + '_' + json_response['data']['lesson']['data']['title'].replace(' ', '_')
-
+        
         for video in json_response['data']['lesson']['data']['lessonParts']['data']:
           video_title = str(video['position']).zfill(2) + '_' + video['title'].replace(' ', '_')
-          video_url = VIMEO_BASE_URL + video['video_id']
-
+          video_url = Constants.VIMEO_BASE_URL + video['video_id']
+          """
           response = requests.get(
             video_url,
             headers = {
@@ -68,12 +64,13 @@ if response.status_code == 200:
             print(video_cdn_url + ',' + lesson_title + '/' + video_title)
           else:
             print(str(response.status_code) + ' ' + video_url)
+      """   
       elif response.status_code == 403:
-        print(str(response.status_code) + ' ' + lesson_url)
+        print(str(response.status_code) + ' Get module (not available) ' + lesson_url)
         break
       else:
-        print(str(response.status_code) + ' ' + lesson_url)
-      """
+        print(str(response.status_code) + ' Get module' + lesson_url)
+  
 else:
-  print(str(response.status_code) + ' GET ' + Constants.API_BASE_URL + Constants.COURSE_NAME)
+  print(str(response.status_code) + ' Get course' + Constants.API_BASE_URL + Constants.COURSE_NAME)
 
