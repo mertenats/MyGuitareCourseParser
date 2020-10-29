c90af1cf7b4a1de60b4cce0328c0427be3d928b3
Samuel Mertenat <samuel.mertenat@gmail.com>
2020-10-29 14:26:56 +0100

    add output file

--- Constants_example.py
+++ Constants_example.py
@@ -1,6 +1,10 @@
-COURSE_NAME       = ''
+COURSE_NAME       	= ''
 
-API_BASE_URL      = ''
-API_BEARER        = ''
+API_BASE_URL      	= ''
+API_BEARER        	= ''
 
-VIMEO_BASE_URL    = 'https://player.vimeo.com/video/'
+VIMEO_BASE_URL    	= 'https://player.vimeo.com/video/'
+
+VIMEO_DOWNLOAD_PATH	= '/home/samuel/Documents/GitHub/MyGuitareCourseParser/vimeo-download/vimeo-download.py'
+
+VIDEO_DOWNLOAD_DST	= '/home/samuel/Downloads/'
